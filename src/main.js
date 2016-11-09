21514c6b568247a20cebc0ea0c98416b608f21be
Jason121250148 <962215093@qq.com>
2016-11-09 15:19:17 +0800

    Build src and public.

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import Snake from "./rs/Snake.js";
+
+const mySnake = new Snake("Jason's snake");
+
+mySnake.sayName();
---
+++ src/rs/Snake.js
@@ -0,0 +1,12 @@
+export default class Snake
+{
+    constructor(name)
+    {
+        this.name = name;
+    }
+
+    sayName()
+    {
+        console.log(this.name);
+    }
+}
