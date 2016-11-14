d5de1fd54c3896dfb3b81fe7cf74644449a67ec7
Jason121250148 <962215093@qq.com>
2016-11-14 15:46:27 +0800

    Clear.

--- README.md
+++
@@ -1,2 +0,0 @@
-# snake-game
-## A simple game of Retro Snaker on nodejs.
--- src/main.js
+++
@@ -1,5 +0,0 @@
-import GamePanel from "./rs/GamePanel.js";
-
-require("./res/main.less");
-
-const gamePanel = new GamePanel(30, 50);
